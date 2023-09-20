936457a50440198b645dccf09683e6ad5f801e52
DanielLepold <daniel@bimspot.io>
2023-09-20 16:26:13 +0200

    readme and handling root folder

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# webserver
+# Web-server in fsharp
+This is a simple web server implementation in fsharp for the following purposes:
+- concurrent client handling
+- restrict file access
+- handling arguments in main
+
+## Concurrent Client Handling
+The primary objective was to implement concurrent client handling.
+This means that this server is able to accept multiple incoming client connections
+and manages them concurrently without blocking one another.
+
+To test the multiple threading, I added 4 sec delay after a request is arrived.
+
+## Restrict file access
+The logic behind is to check whether the requested path provided by the client falls within the "www" directory or its subdirectories.
+If the requested path is outside of this designated area, I return a 404 Not Found response.
+This restriction ensures that only authorized files are served.
+
+## Handling arguments in main
+When developing console applications, it's often necessary to pass arguments
+or configuration parameters to the program.
+In F# and many other programming languages,
+this is commonly done by handling command-line arguments in the main function.
+
+## Usage
+To start the application use the following syntax in the folder of the webserver.exe file:
+- ./webserver root
+
+I added the "www" directory content to my "root" folder.
+
+But you can add any public www directory you like:
+Start the server with your own "www" directory located at for example /var/www/public
+- ./webserver /var/www/public
+
+After the application is running, you can reach and
+test the requests for example with the following commands:
+
+- curl http://localhost:8080/index.html - 200 Ok response
+- curl http://localhost:8080/hello - 404 Not found response
+
+For representation I just added a simple web page into the index.html file.
--- webserver/Program.fs
+++ webserver/Program.fs
@@ -5,15 +5,20 @@ open System.Net.Sockets
 open System.Threading
 
 let getRequestPath (requestLine: string) =
-        let parts = requestLine.Split [|' '|]
-        if parts.Length >= 2 then
-            parts.[1]
-        else
-            "/"
-let asyncHandleClient (client: TcpClient) =
+    let parts = requestLine.Split [|' '|]
+    if parts.Length >= 2 then
+        parts.[1]
+    else
+        "/"
+
+let isPathValid (requestedPath: string) (wwwDirectory: string) =
+    let fullRequestedPath = Path.Combine(wwwDirectory, requestedPath.TrimStart('/'))
+    fullRequestedPath.StartsWith(wwwDirectory) && File.Exists(fullRequestedPath)
+
+let asyncHandleClient (client: TcpClient) wwwDirectory =
     async {
         let threadId = Thread.CurrentThread.ManagedThreadId
-        Console.WriteLine($"Thread: {threadId} is handling the client.")
+        Console.WriteLine($"Thread {threadId} is handling the client.")
 
         let stream = client.GetStream()
         let reader = new StreamReader(stream)
@@ -25,43 +30,61 @@ let asyncHandleClient (client: TcpClient) =
             | null ->
                 Console.WriteLine("Received an empty request.")
             | _ ->
-                // Simulate a 20-second delay
+                // Simulate a 4-second delay
                 do! Async.Sleep(TimeSpan.FromSeconds(4.0))
 
                 let path = getRequestPath requestLine
-                let response =
-                    match path with
-                    | "/hello" -> "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, World!"
-                    | "/"
-                    | "/index.html" ->
-                        // Read the content of the HTML file and send it as the response
-                        let htmlContent = System.IO.File.ReadAllText("index.html")
-                        $"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n{htmlContent}"
-                    | _ -> "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n404 - Not Found"
-                Console.WriteLine($"Thread: {threadId} is sending the response back to the client... {response}.")
-                writer.WriteLine(response)
+                match isPathValid path wwwDirectory with
+                | true ->
+                    let fullPath = Path.Combine(wwwDirectory, path.TrimStart('/'))
+                    let contentType =
+                        if fullPath.EndsWith(".html") then "text/html"
+                        else if fullPath.EndsWith(".txt") then "text/plain"
+                        else "application/octet-stream"  // Default content type
+
+                    let response =
+                        let content = File.ReadAllText(fullPath)
+                        $"HTTP/1.1 200 OK\r\nContent-Type: {contentType}\r\n\r\n{content}"
+
+                    Console.WriteLine($"Thread {threadId} is sending the response back to the client... {response}.")
+                    writer.WriteLine(response)
+                | false ->
+                    let notFoundResponse = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n404 - Not Found"
+                    Console.WriteLine($"Thread {threadId} is sending a 404 response... {notFoundResponse}.")
+                    writer.WriteLine(notFoundResponse)
+
                 writer.Flush()
         with
-            | ex -> Console.WriteLine($"An error occurred: {ex.Message}")
+            | ex -> Console.WriteLine($"An error occurred on thread {threadId}: {ex.Message}")
 
-        Console.WriteLine("Closing client...")
+        Console.WriteLine($"Thread {threadId} is closing the client...")
         client.Close()
     }
 
 [<EntryPoint>]
 let main args =
-    let ipAddress = IPAddress.Parse("127.0.0.1")
-    let port = 8080
-    let listener = new TcpListener(ipAddress, port)
-    listener.Start()
-    Console.WriteLine("Server is listening on port 8080...")
+    match args.Length  with
+    | 1 ->
+        let wwwDirectory = args.[0]
 
-    while true do
-        let client = listener.AcceptTcpClient()
-        async {
-            // Handle each client asynchronously
-            let! _ = Async.StartChild(asyncHandleClient client)
-            return ()
-        } |> Async.Start
+        match (Directory.Exists wwwDirectory) with
+        | false ->
+            Console.WriteLine("The specified www directory does not exist.")
+        | true ->
+            let ipAddress = IPAddress.Parse("127.0.0.1")
+            let port = 8080
+            let listener = new TcpListener(ipAddress, port)
+            listener.Start()
+            Console.WriteLine($"Server is listening on port: {port}...")
+
+            while true do
+                let client = listener.AcceptTcpClient()
+                async {
+                    // Handle each client asynchronously using Async.StartChild
+                    let! _ = Async.StartChild(asyncHandleClient client wwwDirectory)
+                    return ()
+                } |> Async.Start
+    | _ ->
+      Console.WriteLine("Usage: webserver.exe <www-directory>")
 
     0 // Exit code
